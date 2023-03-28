d62cce34188931c2b515c4c55f21d9de6aa8b27c
Jordan <jordansumardi@hotmail.com>
2023-03-28 15:10:12 +0700

    day 4

---
+++ day 4/olx/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+ return (
+  <>
+   <Navbar />
+   <div></div>
+  </>
+ );
+}
+
+export default App;
