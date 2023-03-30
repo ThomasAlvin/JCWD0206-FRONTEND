71bd8652bfaedc0fa710f5392e082c45a8ba39f6
Jordan <jordansumardi@hotmail.com>
2023-03-30 11:27:02 +0700

    todolist

--- day 4/olx/src/components/Products.jsx
+++ day 4/olx/src/components/Products.jsx
@@ -1,5 +1,5 @@
 export function Products() {
- const data = [
+ const selengkapnya = [
   {
    url: 'https://apollo-singapore.akamaized.net/v1/files/fbtl120e4dpu-ID/image;s=272x0',
    price: 575000000,
@@ -46,6 +46,18 @@ export function Products() {
    highlight: false
   },
   {
+   url: 'https://apollo-singapore.akamaized.net/v1/files/qq117p3ibpmn3-ID/image;s=780x0;q=60',
+   price: 2650000000,
+   year: 2022,
+   name: 'BMW 840i Coupe Msport 2023 NIK',
+   location: 'Cilandak, Jakarta Selatan',
+   date: '16 Mar',
+   highlight: false
+  }
+ ];
+
+ const data = [
+  {
    url: 'https://apollo-singapore.akamaized.net/v1/files/p9sl9tf6oup2-ID/image;s=780x0;q=60',
    price: 2745000000,
    year: 2020,
@@ -89,18 +101,169 @@ export function Products() {
    location: 'Batam',
    date: '16 Mar',
    highlight: false
+  },
+  {
+   url: 'https://apollo-singapore.akamaized.net/v1/files/epvrhhrxk8b33-ID/image;s=780x0;q=60',
+   price: 120000000,
+   year: 2001,
+   name: '‼️FOR SALE‼️',
+   location: 'Batam',
+   date: '16 Mar',
+   highlight: false
+  },
+  {
+   url: 'https://apollo-singapore.akamaized.net/v1/files/epvrhhrxk8b33-ID/image;s=780x0;q=60',
+   price: 120000000,
+   year: 2001,
+   name: '‼️FOR SALE‼️',
+   location: 'Batam',
+   date: '16 Mar',
+   highlight: false
+  },
+  {
+   url: 'https://apollo-singapore.akamaized.net/v1/files/epvrhhrxk8b33-ID/image;s=780x0;q=60',
+   price: 120000000,
+   year: 2001,
+   name: '‼️FOR SALE‼️',
+   location: 'Batam',
+   date: '16 Mar',
+   highlight: false
+  },
+  {
+   url: 'https://apollo-singapore.akamaized.net/v1/files/epvrhhrxk8b33-ID/image;s=780x0;q=60',
+   price: 120000000,
+   year: 2001,
+   name: '‼️FOR SALE‼️',
+   location: 'Batam',
+   date: '16 Mar',
+   highlight: false
+  },
+  {
+   url: 'https://apollo-singapore.akamaized.net/v1/files/epvrhhrxk8b33-ID/image;s=780x0;q=60',
+   price: 120000000,
+   year: 2001,
+   name: '‼️FOR SALE‼️',
+   location: 'Batam',
+   date: '16 Mar',
+   highlight: false
+  },
+  {
+   url: 'https://apollo-singapore.akamaized.net/v1/files/epvrhhrxk8b33-ID/image;s=780x0;q=60',
+   price: 120000000,
+   year: 2001,
+   name: '‼️FOR SALE‼️',
+   location: 'Batam',
+   date: '16 Mar',
+   highlight: false
   }
  ];
  return (
   <>
    <div className="banner"></div>
+   <div className="flex" style={{ justifyContent: 'center' }}>
+    <div
+     className="flex"
+     style={{ flexDirection: 'column', alignItems: 'center' }}
+    >
+     <div
+      className="flex"
+      style={{
+       backgroundColor: '#ebeeef',
+       width: '100%',
+       padding: '15px 0px',
+       justifyContent: 'space-between'
+      }}
+     >
+      <div
+       style={{
+        fontSize: '24px',
+        padding: '0px 20px'
+       }}
+      >
+       Selengkapnya tentang mobil anda
+      </div>
+      <div
+       className="flex"
+       style={{
+        fontSize: '14px',
+        fontWeight: 600,
+        textDecoration: 'underline',
+        alignItems: 'center',
+        padding: '0px 20px'
+       }}
+      >
+       Lihat lainnya
+      </div>
+     </div>
+     {/* 
+     <i
+      className="fa fa-angle-left"
+      style={{
+       border: '1px solid black',
+       padding: '5px',
+       position: 'absolute',
+       fontSize: '40px',
+       zIndex: 2,
+       marginTop: '150px'
+      }}
+     />
+     <i
+      className="fa fa-angle-right"
+      style={{
+       border: '1px solid black',
+       padding: '5px',
+       position: 'absolute',
+       fontSize: '40px',
+       zIndex: 2,
+       marginTop: '150px',
+       marginLeft: '100vw'
+      }}
+     /> */}
+     <div className="tentang">
+      {/* <i
+       className="fa fa-angle-left flex"
+       style={{
+        border: '1px solid black',
+        padding: '5px',
+        // position: 'absolute',
+        fontSize: '40px',
+        zIndex: 2,
+        alignItems: 'center'
+        // marginTop: '150px'
+       }}
+      /> */}
+      <div
+       className="products overflowX"
+       style={{ flexWrap: 'nowrap', border: '20px solid #ebeeef' }}
+      >
+       {selengkapnya.map((val, idx) => (
+        <Card key={'product_' + idx} {...val} number={idx + 1} />
+       ))}
+      </div>
+      {/* <i
+       className="fa fa-angle-right flex"
+       style={{
+        border: '1px solid black',
+        padding: '5px',
+        // position: 'absolute',
+        fontSize: '40px',
+        zIndex: 2,
+        alignItems: 'center'
+
+        // marginTop: '150px'
+       }}
+      /> */}
+     </div>
+    </div>
+   </div>
+
    <div
     className="flex"
     style={{
      justifyContent: 'center',
      padding: '10px 0px 20px 0px',
      alignItems: 'center',
-     width: '100vw',
+     width: '100%',
      display: 'flex',
      flexDirection: 'column',
      fontFamily:
@@ -108,10 +271,26 @@ export function Products() {
     }}
    >
     <div className="recommend">Rekomendasi Baru</div>
+
     <div className="products">
      {data.map((val, idx) => (
       <Card key={'product_' + idx} {...val} number={idx + 1} />
      ))}
+    </div>
+
+    <div className="flex" style={{ justifyContent: 'center', padding: '30px' }}>
+     <div
+      style={{
+       padding: '10px',
+       border: '2px solid black',
+       borderRadius: '2px',
+       fontWeight: 600,
+       fontSize: '16px',
+       cursor: 'pointer'
+      }}
+     >
+      Muat lainnya
+     </div>
     </div>
    </div>
   </>
@@ -134,7 +313,7 @@ function Card(props) {
 
    <div
     className={'border-yellow'}
-    style={props.number > 4 ? { borderColor: 'white' } : null}
+    style={props.number > 4 ? { border: '0px' } : null}
    >
     <div className="sub-product">
      <div className="product-harga">
--- day 4/olx/src/pages/TodoList.jsx
+++ day 4/olx/src/pages/TodoList.jsx
@@ -1,19 +1,150 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 export default function TodoListPage() {
+ const [todoList, setTodoList] = useState([]);
+ const [todo, setTodo] = useState({ date: '', judul: '', desc: '' });
+
+ const [total, setTotal] = useState(0);
+
+ function deleteTodo(idx) {
+  const tempArr = [...todoList]; //shallow copy todolist
+  tempArr.splice(idx, 1);
+  setTodoList(tempArr);
+ }
+
+ function inputHandler(event) {
+  const id = event.target.id; //date judul
+  const value = event.target.value;
+  const tempObj = { ...todo }; // {date: null, judul: "", desc : ""} /{date: "2023/03/30", judul : "", desc: ""}
+  tempObj[id] = value; // tempObj['date'] = value, tempObj['judul'] = "makan siang"
+  //{date: "2023/03/30", judul : "", desc: ""}  /{date: "2023/03/30", judul : "makan siang", desc: ""}
+  setTodo(tempObj);
+  console.log(tempObj);
+ }
+
+ useEffect(() => {
+  setTotal(todoList.length);
+ }, [todoList, todo.judul]);
+
+ //useEffect punya 2 arrg
+ // 1. cb
+ // 2. array
+
+ //useEffect akan membaca arrg ke 2 yaitu si array
+ // di dalam array ada variable todolist
+ // apabila todolist isinya berubah
+ //maka dia akan menjalan arrg ke 1
+
+ function add() {
+  const { judul, date, desc } = todo;
+  if (!judul || !date || !desc) {
+   return alert('isi semua dong bos');
+  }
+  const tempArr = [...todoList]; //shallow copy isi array todo list ke tempArr
+  tempArr.push(todo); //Push todo { date,judul,desc} => array=> [{date,judul,desc}]
+  setTodoList(tempArr);
+
+  setTodo({ date: '', judul: '', desc: '' });
+ }
+
  return (
   <center>
+   <h1 style={{ color: 'black' }}>TODO LIST</h1>
    <div
     style={{
      width: '400px',
-     border: '2px solid black',
+     border: '8px double white',
      padding: '5px',
      marginTop: '20px',
-     height: '500px'
+     height: '500px',
+     backgroundColor: 'grey',
+     flexDirection: 'column',
+     display: 'flex',
+     gap: '5px'
     }}
    >
-    <input type="date" name="" id="" />
-    <input style={{ backgroundColor: 'blue', color: 'white' }}></input>
-    <button>add</button>
+    <input
+     style={{ height: '28px' }}
+     type="date"
+     id="date"
+     onChange={inputHandler}
+     value={todo.date}
+    />
+    <input
+     id="judul"
+     style={{ height: '20px' }}
+     placeholder="Judul kegiatan anda ..."
+     onChange={inputHandler}
+     value={todo.judul}
+    ></input>
+
+    <input
+     id="desc"
+     style={{ height: '50px' }}
+     placeholder="Deskripsi kegiatan anda ..."
+     onChange={inputHandler}
+     value={todo.desc}
+    ></input>
+
+    <button
+     style={{
+      height: '30px',
+      border: '2px solid darkgrey',
+      color: 'black',
+      fontWeight: 'bold'
+     }}
+     onClick={add}
+    >
+     ADD
+    </button>
+
+    <div
+     style={{
+      backgroundColor: 'white',
+      width: '400px',
+      padding: '10px 0px',
+      height: '100%',
+      display: 'flex',
+      gap: '5px',
+      flexDirection: 'column',
+      overflowY: 'auto'
+     }}
+    >
+     {todoList.map((val, idx) => (
+      <Card {...val} delete={() => deleteTodo(idx)} />
+     ))}
+    </div>
+   </div>
+   <div>
+    <h1>Total List : {total}</h1>
    </div>
   </center>
+ );
+}
+
+function Card(props) {
+ return (
+  <div
+   style={{
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '10px 10px',
+    gap: '5px',
+    border: '1px solid black',
+    height: '50px'
+   }}
+  >
+   <div style={{ width: '150px', padding: '0px 5px', fontWeight: 'bold' }}>
+    {props.judul.length > 10
+     ? props.judul.substring(0, 10) + '...'
+     : props.judul}
+   </div>
+   <div style={{ width: '150px', padding: '0px 5px' }}> {props.date}</div>
+   <div style={{ width: '150px', padding: '0px 5px' }}>
+    {props.desc.length > 10 ? props.desc.substring(0, 10) + '...' : props.desc}
+   </div>
+   <button onClick={props.delete}>delete</button>
+  </div>
  );
 }
